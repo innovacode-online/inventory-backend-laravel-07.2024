2107a3a601c774e68759ce70dbf034da39b416ff
Ale <duartemattias4@gmail.com>
2024-08-16 22:08:43 -0400

    add protected routes

--- routes/api.php
+++ routes/api.php
@@ -24,7 +24,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::post("/auth/login", [AuthController::class, "login"]);
 
-// Route::middleware(["auth:sanctum", "role.admin"])->group(function () {
+Route::middleware(["auth:sanctum", "role.admin"])->group(function () {
 
     Route::apiResource("categories", CategoryController::class);
 
@@ -39,4 +39,4 @@ Route::post("/auth/login", [AuthController::class, "login"]);
         Route::post('/auth/logout', 'logout');
         Route::get('/auth/checkToken', 'checkToken');
     });
-// });
+});
